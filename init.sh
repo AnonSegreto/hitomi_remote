34177fa66067413de67a7bdc62da2cf289715342
Segreto Anon <anon_segreto@protonmail.ch>
2025-08-03 20:52:41 +0900

    Fix: remove venv from docker

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 ln -s /data ./dest
-python3 -m venv venv
-source ./venv/bin/activate
 pip install -r requirements.txt
 chmod +x ./production.sh
--- production.sh
+++ production.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-source ./venv/bin/activate
 uvicorn main:app --host 0.0.0.0 --port 9198
