f454b60686c4d0ad55e235cc6c758ad325b0f134
Anon Segreto <anon_segreto@protonmail.ch>
2025-08-03 18:14:07 +0900

    Chore: requirements and dependencies

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ln -s /data ./dest
-pip install -r requirements.txt
-python -m venv venv
+python3 -m venv venv
 source ./venv/bin/activate
+pip install -r requirements.txt
 chmod +x ./production.sh
